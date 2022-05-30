57e93ae7a8f70c02b4ebf39f046ff0bf8c27dd2c
Emerson Andrey <emersonandrey_7@hotmail.com>
2022-05-30 17:54:55 -0300

    autenticate endpoint

--- app/includes/autenticate.php
+++ app/includes/autenticate.php
@@ -5,10 +5,11 @@ require_once '../includes/token.php';
 session_start();
 
 $auth_token = (
-    isset($headers['Authorization']) ? $headers['Authorization'] : (
     isset($_SESSION['token']) ? $_SESSION['token'] : (
+    isset($_GET['token']) ? $_GET['token'] : (
     isset($_POST['token']) ? $_POST['token'] : (
-    isset($_GET['token']) ? $_GET['token'] : ''))));
+    isset($_PUT['token']) ? $_PUT['token'] : (
+    isset($_DELETE['token']) ? $_DELETE['token'] : '')))));
 
 
 if($auth_token !== $_ENV['secret_token']) {
